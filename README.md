68b0512d6a64c46f43689aaca8beb8ece42e74ff
John Gantner <jayrgee@fastmail.fm>
2017-02-06 13:15:04 +1100

    update mod2 solution

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ coursera single-page-web-apps-with-angularjs
 https://www.coursera.org/learn/single-page-web-apps-with-angularjs/home/welcome
 
 http://jayrgee.github.io/coursera-spwa/module1-solution/
+
+http://jayrgee.github.io/coursera-spwa/module2-solution/
---
+++ module2-solution/app.js
@@ -0,0 +1,56 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController (ShoppingListCheckOffService) {
+
+  var itemsToBuy = this;
+
+  itemsToBuy.items = ShoppingListCheckOffService.getItemsToBuy();
+
+  itemsToBuy.checkOffItem = function (itemIndex) {
+    ShoppingListCheckOffService.checkOffItem(itemIndex);
+  }
+
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController (ShoppingListCheckOffService) {
+
+  var itemsBought = this;
+
+  itemsBought.items = ShoppingListCheckOffService.getItemsBought();
+}
+
+function ShoppingListCheckOffService () {
+  var service = this,
+    itemsToBuy = [
+      { name: "apples", quantity: 9 },
+      { name: "oranges", quantity: 8 },
+      { name: "lemons", quantity: 7 },
+      { name: "limes", quantity: 6 },
+      { name: "pears", quantity: 5 }
+    ],
+    itemsBought = [];
+
+  service.getItemsToBuy = function () {
+    return itemsToBuy;
+  };
+
+  service.getItemsBought = function () {
+    return itemsBought;
+  };
+
+  service.checkOffItem = function (itemIndex) {
+    var item = itemsToBuy[itemIndex];
+    itemsToBuy.splice(itemIndex, 1);
+    itemsBought.push(item);
+  };
+}
+
+})();
